8ba4530a65d8bb167321c74c5a25ee2e6e411503
LucasOs4kwe <lucas@vecta.com.br>
2021-11-24 15:02:55 -0300

    Site Individual

--- Desenvolvimento Socioemocional/api-site/src/database/sql.sql
+++ Desenvolvimento Socioemocional/api-site/src/database/sql.sql
@@ -4,9 +4,34 @@ CREATE DATABASE gamezone;
 USE gamezone;
 
 CREATE TABLE usuario (
+	idUsuario INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(70) NOT NULL,
+	email VARCHAR(70) UNIQUE NOT NULL,
+	senha VARCHAR(50) NOT NULL,
+	nick VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE triste (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(70),
-	email VARCHAR(70),
-	senha VARCHAR(50),
-	nick VARCHAR(50)
+	qntd decimal(5)
+);
+
+CREATE TABLE feliz (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	qntd decimal(5)
+);
+
+CREATE TABLE amei (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	qntd decimal(5)
+);
+
+CREATE TABLE uau (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	qntd decimal(5)
+);
+
+CREATE TABLE grr (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	qntd decimal(5)
 );
